484302ffd33b509053bb4fc7040de8995f5cba7c
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-29 01:14:04 +0100

    welcome count works

--- src/common/pages/PageWelcome.tsx
+++ src/common/pages/PageWelcome.tsx
@@ -1,13 +1,17 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { add, subtract } from "../../features/cart/cartSlice";
+import { RootState } from '../../app/store';
 
 export const PageWelcome = () => {
 	const dispatch = useDispatch();
+	const selectCart= (state: RootState) => state.cart;
+	const { count } = useSelector(selectCart);
 
 	return (
 		<div className="flex gap-3">
-			<button>Add</button>
-			<button>Subtract</button>
+			<button onClick={() => dispatch(subtract())}>Subtract</button>
+			<button onClick={() => dispatch(add())}>Add</button>
+			<div className="text-3xl">count = {count}</div>
 		</div>
 	);
 };
